4e21f1ffa9f7b74c2bffaac5f80a1263d77e28ea
jenish ghimire <j.ghimire555@gmail.com>
2022-07-27 08:45:39 -0500

    Quiz completed

---
+++ Lab-8/ascii.js
@@ -0,0 +1,15 @@
+// $ (document ).ready(function(){
+//     $("button").click (function(){
+//         $(".start")
+//     })
+
+
+// })
+
+$(document).ready(function(){
+    $("#h1").on({
+        mouseenter: function(){
+            $(this).css("background-color", "blue")
+        }
+    });
+});
