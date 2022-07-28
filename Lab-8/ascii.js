04841a6aebe51dd9c08e72904058d91a3d894c49
jenish ghimire <j.ghimire555@gmail.com>
2022-07-27 23:57:32 -0500

    Initial complete

--- Lab-8/ascii.js
+++ Lab-8/ascii.js
@@ -1,15 +1,70 @@
-// $ (document ).ready(function(){
-//     $("button").click (function(){
-//         $(".start")
-//     })
+
+let counter = 0;
+let timerInterval;
+let delayMs = 250;
+let frames;
+let frameArr;
+
+$(document).ready(function () {
 
 
-// })
+    $("#stop").prop("disabled", true);
 
-$(document).ready(function(){
-    $("#h1").on({
-        mouseenter: function(){
-            $(this).css("background-color", "blue")
+    $("#start").click(function () { 
+        startAnimation();
+        
+    });
+
+    $("#sizeSelect").change(function () {
+        if ($("#start").prop("disabled")) {
+            startAnimation();
         }
     });
+
+    $("#animationselect").change(function () {
+        if ($("#start").prop("disabled")) {
+            startAnimation();
+        }
+    });
+
+    $('#speed').change(function () {
+        startAnimation();
+    })
 });
+
+function startAnimation() {
+    let animationselect = $("#animationselect").val();
+    frames = ANIMATIONS[animationselect];
+    frameArr = frames.split("=====\n");
+    playAnimation();
+    changesizeSelect();
+    $("#start").prop("disabled", true);
+    $("#stop").prop("disabled", false);
+}
+
+function changesizeSelect() {
+    const sizeSelect = $("#sizeSelect").val();
+    $("#textWriter").css("fontsizeSelect", sizeSelect);
+}
+
+
+function playAnimation() {
+    clearInterval(timerInterval);
+    var isTurbo = $("#speed").prop("checked");
+
+    if (isTurbo) {
+        delayMs = 50;
+    } else {
+        delayMs = 250;
+    }
+    timerInterval = setInterval(animateDiagram, delayMs);
+}
+
+function animateDiagram() {
+    if (counter >= frameArr.length) {
+        counter = 0;
+    } else {
+        $("#textWriter").val(frameArr[counter]);
+        counter++;
+    }
+}
