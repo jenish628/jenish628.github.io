dcf4a30540dec7f0bc1130a41f83170759a3a7d3
jenish ghimire <j.ghimire555@gmail.com>
2022-08-03 08:48:26 -0500

    Completed Calculator task

--- Node-2/index.js
+++ Node-2/index.js
@@ -1,4 +1,68 @@
-const  express = require('express')
+
+
+const express = require('express')
 const bodyParser = require('body-parser')
-const app = express ()
+const path = require('path');
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+
+app.get("/",(req,resp) => {
+    const dirName = path.dirname(__dirname)
+    console.log(dirName)
+    resp.sendFile(`${dirName}/Node-2/calc.html`)
+
+})
+
+
+const getHtmlValue = (value) => {
+    return `
+        <div>
+            Your result is : ${value} <br/>
+
+            Goto Home Page <a href ="http://localhost:8000">GoToHome Page</a>
+        </div>
+    
+    `
+}
+
+app.post("/calculator",(req,resp) => {
+
+
+    const num1 = parseInt(req.body.first)
+    const num2 = parseInt(req.body.second)
+    const operation = req.body.operationName
+
+
+    switch(operation) {
+
+        case "add":
+            resp.send(getHtmlValue(num1+num2))
+        break
+
+        case "sub":
+            resp.send(getHtmlValue(num1-num2))
+            break;
+
+        case "mul":
+            resp.send(getHtmlValue(num1*num2))
+            break;
+        case "divide":
+            resp.send(getHtmlValue(num1/num2))
+            break;
+    }
+
+
+
+
+
+})
+
+
+
+
+
+app.listen(8000)
 
